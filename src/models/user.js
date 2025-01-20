5eeccc7d633b1a14ba622dfac78100607db6950f
Unik-Dahal <unikdahal03@gmail.com>
2025-01-20 22:22:28 +0530

    User Model Created

---
+++ src/models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  gender: {
+    type: String,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
